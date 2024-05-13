80d73b40af1d7679c77fa8447e6eada8ba327c15
JChaverra21 <jose_chaverra82182@elpoli.edu.co>
2024-05-13 12:55:20 -0500

    Se comenta el funcionamiento de los CORS y el redirect de los nanolinks

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ import "dotenv/config";
 import "./database/connectdb.js";
 import cookieParser from "cookie-parser";
 import express from "express";
-import cors from "cors";
+//import cors from "cors";
 
 import authRoute from "./routes/auth.route.js";
 import linkRoute from "./routes/link.route.js";
@@ -11,9 +11,9 @@ import redirectRouter from "./routes/redirect.route.js";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const whiteList = [process.env.ORIGIN1, process.env.ORIGIN2];
+//const whiteList = [process.env.ORIGIN1, process.env.ORIGIN2];
 
-app.use(
+/* app.use(
   cors({
     origin: function (origin, callback) {
       console.log("😎😎😎 =>", origin);
@@ -25,18 +25,18 @@ app.use(
       );
     },
   })
-);
+); */
 
 app.use(express.json());
 app.use(cookieParser());
 
 // Ejemplo back redirect (opcional)
-app.use("/", redirectRouter);
+//app.use("/", redirectRouter);
 
 app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/links", linkRoute);
 
 // Solo para ejemplo de Login/token
-//app.use(express.static('public'));
+app.use(express.static('public'));
 
 app.listen(PORT, () => console.log("🚀 http://localhost:" + PORT));
